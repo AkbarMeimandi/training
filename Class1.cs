a6823e0585a2ea1468bf5777fbb5f67b7f72c994
AkbarMeimandi <meimandi.akbar@yahoo.com>
2025-01-31 17:03:52 +0330

    Class1 is Added

---
+++ Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    public class Class1
+    {
+        public static void myPrint(string str)
+        {
+            Console.WriteLine(str);
+        }
+
+        public void MyOutText(string str)
+        {
+
+            myPrint(str);
+        }
+    }
+}
