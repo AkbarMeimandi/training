2fb1b54dfa1da23a0a4b8494cd386b5bfb0a3636
AkbarMeimandi <meimandi.akbar@yahoo.com>
2025-01-31 17:01:09 +0330

    Program File Added

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleApp3;
+
+Console.WriteLine("Hello, World!");
+
+Class1 obj1 = new Class1();
+
+obj1.MyOutText("This my Class Printing!");
