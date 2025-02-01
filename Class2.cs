55f9126d7ee626587340b10d1aaef03dc332b777
AkbarMeimandi <meimandi.akbar@yahoo.com>
2025-02-01 16:38:41 +0330

    Before Push

--- Class2.cs
+++ Class2.cs
@@ -8,5 +8,14 @@ namespace ConsoleApp3
 {
     internal class Class2
     {
+        public static void myPrint(string str)
+        {
+            Console.WriteLine(str);
+        }
+
+        public void MyOutText(string str)
+        {
+            myPrint(str);
+        }
     }
 }
